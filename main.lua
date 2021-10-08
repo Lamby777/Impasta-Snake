ee59e62576b95dad4ee0177ca4301081434f541f
Dexie <35111165+Lamby777@users.noreply.github.com>
2021-10-08 18:44:25 +0000

    snake table

--- main.lua
+++ main.lua
@@ -1,6 +1,30 @@
 -- Constants
 local BOARD_WIDTH = 10
 local BOARD_HEIGHT = 10
+local BOARD_SIZE = BOARD_WIDTH * BOARD_HEIGHT
 
 -- Game board
 local board = {}
+
+-- Set snake position to middle
+local snake = {
+	math.floor(BOARD_SIZE/2),
+	math.floor(BOARD_SIZE/2) - 1,
+	math.floor(BOARD_SIZE/2) - 2,
+}
+
+local game = true
+while (game) do
+	-- Make board
+	--
+
+	-- Print board
+	print({
+		--
+	})
+	io.read()
+end
+
+function makeBoard()
+	--
+end
